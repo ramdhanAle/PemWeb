3effb65f95b80c482d0e44155b6936098057fcfc
ramdhanAle <leeslowly32@gmail.com>
2026-05-16 02:46:04 +0700

    pert1

--- pert4-5-6-7/rwd/models/produk_cari.php
+++
@@ -1,6 +0,0 @@
-<?php
-$ar_judul = ['NO', 'KODE', 'NAMA', 'KONDISI', 'HARGA', 'STOK', 'FOTO', 'ACTION'];
-$obj_produk = new Produk();
-$keyword = $_GET['keyword'];
-$rs = $obj_produk->cari($keyword);
-?>
